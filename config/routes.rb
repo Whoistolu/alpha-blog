9bd6ffb69b60c1d244486a1fb7d7d9e32143d483
Whoistolu <digittolu25@gmsil.com>
2023-03-05 20:56:59 +0100

    Add a file for method tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   # root 'pages#home'
   # get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :create, :new]
+  resources :articles, only: [:show, :index, :create, :new, :edit, :update]
 end
---
+++ methods.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+z = [dd, ff, gg]
+z.each { |food| puts food, + ' ' }
